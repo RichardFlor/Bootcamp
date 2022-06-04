ae4d4b070e191f85c9353cddbf660a72cf99618b
RichardFlor <richardflor2015@gmail.com>
2022-06-03 23:35:55 -0300

    Operções Lógicas

--- Aula 06 JAVA/src/Aula06.java
+++ Aula 06 JAVA/src/Aula06.java
@@ -4,9 +4,40 @@ public class Aula06 {
         /*
             && AND (E)
             || OR (OU)
-            ! NOT (NEGAÇÃO, NÃO)
+            ! NOT (NEGAÇÃO, NÃO) inverte!!
+         */
+        /*    TABELA VERDADE 
+               A         B          A&&B        A || B          !
+             true      false        false        true           
+             true      true         true         true
+             false     false        false        false
+             false     true         false        true
+        
          */
 
-         
+         //Credencias para logar 
+         String usuario = "Meu git";
+         String senha = "123";
+
+         //Usuario digitou na tela de login
+         String usuarioDigitado = "Meu git";
+         String senhaDigitada = "123"; 
+         //
+
+        // &&
+         boolean loginCorreto = usuarioDigitado == usuario && senhaDigitada == senha;
+         System.out.println(loginCorreto);
+
+        // ||
+         int idUsuario = 10;
+         boolean numero = idUsuario == 10 || idUsuario == 5;
+         System.out.println(numero);
+
+        // !
+        boolean resultado = !(usuarioDigitado == usuario);
+        System.out.println("Resultado: " + resultado);
+
+
+
     }
 }
