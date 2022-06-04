6e6f97947af9a77fe8c3f6fb67688f7616476d5a
RichardFlor <richardflor2015@gmail.com>
2022-06-03 23:11:59 -0300

    Operadores Relacionais

--- Aula 05 JAVA/src/Aula05.java
+++ Aula 05 JAVA/src/Aula05.java
@@ -2,18 +2,34 @@ import javax.swing.JOptionPane;
 
 public class Aula05 {
     public static void main(String[] args) throws Exception {
-        int idade = 20;
-        /*
+         /*
             > maior que
             < menor que
             >= maior ou igual a
             <= menor ou igaul a 
             == igual a
             != diferente de 
+
+            *****  um igual ( = )  é atribuição 
          
          */
-        boolean resultado = idade >= 18;
-        JOptionPane.showMessageDialog(null, resultado);
-       
+        /* 
+            int idade = 20;
+            boolean resultado = idade >= 18;
+            JOptionPane.showMessageDialog(null, resultado);
+       */
+
+        /* 
+            String nome = "Richard";
+            boolean resultado = nome == "Matheus";
+            JOptionPane.showMessageDialog(null, resultado);
+        */
+
+        int episodiosVistos = 420;
+        int totalEpisodios = 500;
+
+        boolean acabou = episodiosVistos == totalEpisodios;
+        JOptionPane.showMessageDialog(null,acabou);
+
     }
 }
