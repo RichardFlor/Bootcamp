97fc08cf3887adb64b7dc648e128463ee460670f
RichardFlor <richardflor2015@gmail.com>
2022-06-03 23:16:00 -0300

    Operadores Lógicos

---
+++ Aula 06 JAVA/src/Aula06.java
@@ -0,0 +1,12 @@
+public class Aula06 {
+    public static void main(String[] args) throws Exception {
+       
+        /*
+            && AND (E)
+            || OR (OU)
+            ! NOT (NEGAÇÃO, NÃO)
+         */
+
+         
+    }
+}
