21afebd970b056b9f88bcca46a64a0259bc2f033
RichardFlor <richardflor2015@gmail.com>
2022-06-06 21:41:41 -0300

    IF, ELSE, ElSE IF e TERNÁRIO

--- Aula 07 JAVA/src/Aula07.java
+++ Aula 07 JAVA/src/Aula07.java
@@ -1,5 +1,59 @@
 public class Aula07 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*
+            > maior que
+            < menor que
+            <= menor ou igual a 
+            >= maior ou igual a 
+            == igual
+            != diferente de
+
+            && - AND (E)
+            || - OR (ou)
+            !  - NOT (NÂO)
+         */
+
+            /* 
+        int poderDeLuta = 2000;
+        if(poderDeLuta > 8000){
+            System.out.println("O poder de luta dele é mais de 8000!");
+        }
+
+        boolean estaChovendo = false;
+        boolean temGuardaChuva = false;
+        // IF - SE
+        if (estaChovendo == true && temGuardaChuva == true){ // Condição 1
+            //Executa tudo que estiver aqui caso seja TRUE
+            System.out.println("Muito bem, nao vai se molhar hoje!");
+        } else if(estaChovendo == true){ // Condição 2
+            //Executa caso a condição anterior for FALSE, e a condeção do ELSE IF for TRUE
+            System.out.println("Você pode brincar na chuva!");
+        }else{ // Condição 3
+            System.out.println("Não esta chovendo!");
+        }
+             */
+
+            
+            /*
+        String corFruta = "vermelho";
+
+        if(corFruta == "vermelho"){
+            System.out.println("Está madura");
+        } else if(corFruta == "amarelo"){
+            System.out.println("Está quase madura");
+        } else if ( corFruta == "verde"){
+            System.out.println("Não esta boa para comer");
+        } else{
+            System.out.println("Não come! está podre");
+        }
+             */
+
+        //Ternário
+        String video = "Overlord";
+        String categoria = (video == "HunterXHunter") ? "anime" : "série";
+     
+            System.out.println(categoria);
+
     }
 }
