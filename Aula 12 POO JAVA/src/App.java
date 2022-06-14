cde4c76b1d59ee2a3b885a80105162d8ff1d22e5
RichardFlor <richardflor2015@gmail.com>
2022-06-14 15:42:49 -0300

    Básico de packages

---
+++ Aula 12 POO JAVA/src/App.java
@@ -0,0 +1,29 @@
+import br.com.devflix.gui.*;
+import br.com.devflix.gui.botoes.*;
+import br.com.devflix.videos.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      Filme filme = new Filme();
+      Novela novela = new Novela();
+      Documentario documentario = new Documentario();
+      Serie serie = new Serie();
+      Anime anime = new Anime();
+
+      Janela janelinha = new Janela();
+      MiniaturaVideo miniatura = new MiniaturaVideo();
+
+      BotaoPlay botaoPlay = new BotaoPlay();
+
+      // localizacao.Gps
+
+      // br.com.localizacao.brasil.Gps
+      //br/ 
+      //  com/
+      //    localizacao/
+      //        brasil/
+      //            Gps
+
+    }
+}
