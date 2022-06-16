dbaac523eb4f0aec192ccb4ce46876bd89859a17
RichardFlor <richardflor2015@gmail.com>
2022-06-15 21:16:20 -0300

    Construtores

--- Aula 15 POO JAVA/src/App.java
+++ Aula 15 POO JAVA/src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JOptionPane;
+
+import classes.Personagem;
+//import classes.Pessoa;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //Pessoa pessoa = new Pessoa("Richard", 21, 1.83f, 60, "Ric", true);
+       //pessoa.imprimirDados();
+
+       String nome = JOptionPane.showInputDialog(null, "Nome do Personagem: ");
+       String classe = JOptionPane.showInputDialog(null, "Classe do personagem: ");
+
+
+       Personagem guerreiro = new Personagem(nome, classe);
+        guerreiro.mostrarStatus();
     }
 }
---
+++ Aula 15 POO JAVA/src/classes/Pessoa.java
@@ -0,0 +1,54 @@
+/*package classes;
+
+public class Pessoa {
+    //Atributos da classe
+    private String nome;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String apelido;
+    private boolean estaCadastrado;
+
+    //Método Construtor
+    //<modificador> <nomeDaClasse>()
+    public Pessoa(String nome, int idade, float altura, float peso){
+        System.out.println("Uma pessoa acaba de nascer!!");
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    
+    }
+
+    public void imprimirDados(){
+        System.out.format("Nome: %s, idade = %d e %.2fm \n",
+        nome,
+        idade,
+        altura);
+    }
+
+
+    // Métodos getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+
+    
+}
+ */
